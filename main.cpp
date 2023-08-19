f7b437d858581dea59d047decc442abd73c52a6e
MintzyG <ericbraga2000@gmail.com>
2023-08-19 10:44:40 -0300

    Removed unecessary difference calculation and return chain The program had a return chain that return the visualization of how close two colors are instead of returning the color to replace the pixel, it resulted in some interesting images but was wrong. The second problem was that the program was calculating the difference twice, and then subtracting them, this had the effect of making (0,0,0) always be the closest color to any color despite it not actually being the closest, resulting in an all black image. Both problems are now fixed and the program works as intended.

--- main.cpp
+++ main.cpp
@@ -10,46 +10,36 @@ using namespace cv;
 
 bool exists (std::vector<Vec3b> parameters, Vec3b color) {
     for (int i = 0; i < parameters.size(); i++) {
-        if (parameters[i] == color) {
+        if (parameters[i] == color) 
             return true;
-        }
-        else if (parameters.empty()) {
+        else if (parameters.empty())
             return false;
-        }
+
         return false;
     }
+    return false;
 }
 
-Vec3b magnitude(Vec3b cor, std::vector<Vec3b> parameters) {
-    int x, y, z, magnitude, lesser = 442, pos = 0;
+Vec3b magnitude(Mat& img, std::vector<Vec3b> parameters, int imgY, int imgX) {
+    Vec3b color = img.at<Vec3b>(imgY, imgX);
+    int x, y, z, magnitude = 442, lesser = 442, pos = 0;
     for (int i = 0; i < parameters.size(); i++) {
-        x = (int)std::pow((double)(parameters[i][0] - cor[0]), 2);
-        y = (int)std::pow((double)(parameters[i][1] - cor[1]), 2);
-        z = (int)std::pow((double)(parameters[i][2] - cor[2]), 2);
-        magnitude = (int)std::sqrt(x + y + z);
+        x = (int)std::pow((double)(parameters[i][0] - color[0]), 2);
+        y = (int)std::pow((double)(parameters[i][1] - color[1]), 2);
+        z = (int)std::pow((double)(parameters[i][2] - color[2]), 2);
+        magnitude = (int)std::sqrt((x + y + z));
         if (magnitude < lesser) {
             lesser = magnitude;
             pos = i;
         }
+        else if (magnitude == 0)
+            return parameters[i];
     }
     return parameters[pos];
 }
 
-Vec3b min(Mat& img, std::vector<Vec3b> parameters, int y, int x) {
-    Vec3b color = img.at<Vec3b>(y, x);
-    for (int i = 0; i < parameters.size(); i++) {
-        if (i < parameters.size()) {
-            parameters[i][0] = std::abs(color[0] - parameters[i][0]);
-            parameters[i][1] = std::abs(color[1] - parameters[i][1]);
-            parameters[i][2] = std::abs(color[2] - parameters[i][2]);
-        }
-        return magnitude(color, parameters);
-    }
-    return color;
-}
-
 int main(int argc, char** argv) {
-    cv::Mat img = cv::imread("C:/Users/sophia/Downloads/frog-1280x720.png");
+    cv::Mat img = cv::imread("C:/Users/sophia/Downloads/Amph_RedEyedTreeFrog.jpg");
     std::srand((int)std::time(0));
     int x = img.cols, y = img.rows, amount = 0;
     std::vector<Vec3b> parameters;
@@ -58,20 +48,17 @@ int main(int argc, char** argv) {
 
     while (parameters.size() < amount) {
         Vec3b color = img.at<Vec3b>(std::rand() % y, std::rand() % x);
-        if (parameters.empty() || !exists(parameters, color)) {
+        if (parameters.empty() || !exists(parameters, color)) 
             parameters.push_back(color);
-        }
     }
 
-    for (int i = 0; i < amount; i++) {
-        for (int y = 0; y < img.rows; y++) {
-            for (int x = 0; x < img.cols; x++) {
-                Vec3b color = min(img, parameters, y, x);
-                img.at<Vec3b>(y, x) = color;
-            }
+    for (int y = 0; y < img.rows; y++) {
+        for (int x = 0; x < img.cols; x++) {
+            Vec3b color = magnitude(img, parameters, y, x);
+            img.at<Vec3b>(y, x) = color;
         }
     }
-
+ 
     namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
     cv::imshow("First OpenCV Application", img);
     cv::moveWindow("First OpenCV Application", 0, 45);
